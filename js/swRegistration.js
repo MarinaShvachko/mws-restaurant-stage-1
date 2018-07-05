f7bb3cfc84050207e74e596ca82915f380b3fe11
Marina_Shvachko <email.for.marina@gmail.com>
2018-07-05 07:52:36 +0500

    works offline, but needs a lot of work still

---
+++ js/swRegistration.js
@@ -0,0 +1,13 @@
+
+
+
+
+  // registration
+  if (navigator.serviceWorker) {
+
+  navigator.serviceWorker.register("/sw.js").then(registration => {
+    console.log("Registration worked!");
+  }).catch(error => {
+    console.log("Registration failed!");
+});
+};
